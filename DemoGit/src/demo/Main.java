c7b1fc7fc5e32bd78aeed6c251ae85d8b3f782ea
Xuanute <xuantrinh812@gmail.com>
2016-09-26 21:38:54 +0700

    test 2

--- DemoGit/src/demo/Main.java
+++ DemoGit/src/demo/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		System.out.println("Hello 2");
 		System.out.println("Hello 3");
 		System.out.println("afbvvvf");
-		System.out.println("new test");
+		System.out.println("abc");
 		
 	}
 
