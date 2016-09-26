794ffc54665c563105da4091fd013444b0dddc43
Xuanute <xuantrinh812@gmail.com>
2016-09-26 21:33:29 +0700

    test commit

---
+++ DemoGit/src/demo/Main.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		data data = new data();
+		displayScreen();
+		data.display();
+	}
+	public static void displayScreen()
+	{
+		System.out.println("hello 1");
+		System.out.println("Hello 2");
+		System.out.println("Hello 3");
+		System.out.println("afbvvvf");
+		System.out.println("new test");
+		
+	}
+
+}
